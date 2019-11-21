a9a26267d3caf9f1fc73cb42f091da6d46fa3cb5
weimatt <weimatt@umich.edu>
2019-11-21 14:04:20 -0500

    Fixed search activity into manifest, edited dollar signs to radio buttons

--- app/src/main/java/com/example/wheretoeat/SearchActivity.java
+++ app/src/main/java/com/example/wheretoeat/SearchActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -16,8 +17,8 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
     Button buttonTellMe_Search;
     TextView textViewHey_Search, textViewSelect_Search, textViewCuisine_Search, textViewPrice_Search, textViewTime_Search;
     Spinner spinnerCuisine_Search;
-    CheckBox checkBoxOneDollar_Search, checkBoxTwoDollar_Search, checkBoxThreeDollar_Search;
-    RadioButton RadioButton5Min_Search, RadioButton10Min_Search, RadioButton20Min_Search;
+    RadioButton radioButton5Min_Search, radioButton10Min_Search, radioButton20Min_Search, radioButtonOneDollar_Search, radioButtonTwoDollar_Search, radioButtonThreeDollar_Search;
+    RadioGroup radioGroupDollar_Search, radioGroupTime_Search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,13 +35,17 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
         textViewTime_Search = findViewById(R.id.textViewTime_Search);
 
         spinnerCuisine_Search = findViewById(R.id.spinnerCuisine_Search);
-        checkBoxOneDollar_Search = findViewById(R.id.checkBoxOneDollar_Search);
-        checkBoxTwoDollar_Search = findViewById(R.id.checkBoxTwoDollar_Search);
-        checkBoxThreeDollar_Search = findViewById(R.id.checkBoxThreeDollar_Search);
 
-        RadioButton5Min_Search = findViewById(R.id.radioButton5Min_Search);
-        RadioButton10Min_Search = findViewById(R.id.radioButton10Min_Search);
-        RadioButton20Min_Search = findViewById(R.id.radioButton20Min_Search);
+        radioGroupDollar_Search = findViewById(R.id.radioGroupDollar_Search);
+        radioGroupTime_Search = findViewById(R.id.radioGroupTime_Search);
+
+        radioButton5Min_Search = findViewById(R.id.radioButton5Min_Search);
+        radioButton10Min_Search = findViewById(R.id.radioButton10Min_Search);
+        radioButton20Min_Search = findViewById(R.id.radioButton20Min_Search);
+
+        radioButtonOneDollar_Search = findViewById(R.id.radioButtonOneDollar_Search);
+        radioButtonTwoDollar_Search = findViewById(R.id.radioButtonTwoDollar_Search);
+        radioButtonThreeDollar_Search = findViewById(R.id.radioButtonThreeDollar_Search);
 
 
     }
