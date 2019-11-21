251030873ff53b80efc593618b48ba175d30ecd3
huangleo <huangleo@umich.edu>
2019-11-21 13:45:35 -0500

    query class

--- app/src/main/java/com/example/wheretoeat/Queries.java
+++ app/src/main/java/com/example/wheretoeat/Queries.java
@@ -1,4 +1,29 @@
 package com.example.wheretoeat;
 
+
+import java.sql.Timestamp;
+
 public class Queries {
+    public Restaurant qResult;
+    public String qCuisineType;
+    public int qPrice;
+    public int qWalkingTime;
+    public Timestamp qTimeStamp;
+    public String qActionTaken;
+
+    public Queries() {
+    }
+
+    public Queries(String qCuisineType, int qPrice, int qWalkingTime, Timestamp qTimeStamp) {
+        this.qCuisineType = qCuisineType;
+        this.qPrice = qPrice;
+        this.qWalkingTime = qWalkingTime;
+        this.qTimeStamp = qTimeStamp;
+    }
+    public void setResult(Restaurant qResult) {
+        this.qResult = qResult;
+    }
+    public void setActionTaken(String qActionTaken) {
+        this.qActionTaken = qActionTaken;
+    }
 }
